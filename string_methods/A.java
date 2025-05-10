8ea2082de7b66b4aa43ffb313ee3f3deb70db7b0
thakursahil999 <thakursahil9302@gmail.com>
2025-05-10 09:02:38 +0530

    string methods

---
+++ string_methods/A.java
@@ -0,0 +1,7 @@
+class A{
+    public static void main(String[] args) {
+        String a="prithviraj";
+        
+        System.out.println(a.charAt(2) + " character at index 2");  //i character at index 2
+    }
+}
